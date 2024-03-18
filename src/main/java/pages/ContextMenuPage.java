b3e239f7b8aeb986788bdba1c358c9ff75630586
Yoana Stefanova <yoana.stefanova@endava.com>
2024-03-18 17:17:52 +0200

    imports cleanup

--- src/main/java/pages/ContextMenuPage.java
+++ src/main/java/pages/ContextMenuPage.java
@@ -1,13 +1,9 @@
 package pages;
 
-import java.time.Duration;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ContextMenuPage {
 
